edd9c423f57ed256f07dfc92fd72af17d6b73aa7
e <1244653559@qq.com>
2019-12-21 00:17:33 +0800

    【安装插件】

--- miniprogram/pages/playlist/index.js
+++ miniprogram/pages/playlist/index.js
@@ -21,46 +21,17 @@ Page({
   onReady: function () {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+async open(){
+  console.log(23243)
+  let res=await this.timeOut()
+  console.log(res)
+},
+  timeOut(){
+    return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        console.log(324324)
+        resolve({aa:23423432})
+      },1000)
+    })
   }
 })
